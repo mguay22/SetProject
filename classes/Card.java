24b55479cdd4077c6d2c643ea319af60251b8769
Michael Guay <>
2017-10-26 14:02:18 -0400

    First commit

---
+++ classes/Card.java
@@ -0,0 +1,19 @@
+package classes;
+
+public class Card {
+  // Instance Variables
+
+  // Set up enums to represent each set of features
+  private enum Shape {
+    OVAL,
+    SQUIGGLES,
+    DIAMONDS
+  }
+
+  private enum Color {
+    RED,
+    PURPLE,
+    GREEN
+  }
+
+}
